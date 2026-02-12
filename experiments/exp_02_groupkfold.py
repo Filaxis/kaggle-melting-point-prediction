cf9c1c206f17b71ac4653d42295edd7cee2b04ba
Filaxis <filaxis@gmail.com>
2026-02-12 09:34:59 +0100

    Delete experiments/exp_02_groupkfold.py

--- experiments/exp_02_groupkfold.py
+++
@@ -1,8 +0,0 @@
-FP_COLS = [f"fp_{i}" for i in range(512)]
-
-DESC_COLS = [
-    "MolWt", "MolLogP", "TPSA",
-    "NumHDonors", "NumHAcceptors",
-    "NumRotatableBonds", "RingCount",
-    "FractionCSP3",
-]
