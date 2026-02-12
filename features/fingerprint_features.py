5d61b62462a12b93839ff19ee0b0f3db2be7be15
Filaxis <filaxis@gmail.com>
2026-02-12 09:46:30 +0100

    Delete features/fingerprint_features.py

--- features/fingerprint_features.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-
-# Load CSV
-df_train = pd.read_csv("melt_train_morgan_fps_512.csv")
-df_ext = pd.read_csv("external_morgan_fps_512.csv")
-
-# Sanity check
-assert list(df_train.columns) == list(df_ext.columns)
